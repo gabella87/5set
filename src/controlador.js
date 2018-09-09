e17808489efe5e9a7f2ca90c37907558f4764b7f
Guille Abella <42720588+gabella87@users.noreply.github.com>
2018-09-09 19:43:24 -0300

    Delete controlador.js

--- src/controlador.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myApp',[]);
-app.controller('control',function($scope, $http){
-    http.post('getGuille.php').success(function(data){
-        $scope.posts = data;
-    });
-});
